e3814f232994e68ea053440b253c8dfc921b545b
daechatorn.namjai <daechatorn.man@gmail.com>
2022-03-31 13:42:43 +0700

    Add produces

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/test/servicenow")
 @Validated
 public class TestController {
-    @PostMapping("/inquiry")
+    @PostMapping(path = "/inquiry", produces= MediaType.APPLICATION_JSON_VALUE)
     public String inquiry(
             @RequestBody String request){
         System.out.println(request);
